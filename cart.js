afed6d62535a466939e3b020d81f828bfbd5cbfc
7rolmstead <7rolmstead@stpatrick.org>
2016-12-12 10:55:04 -0600

    finished item, working on functions in cart

---
+++ cart.js
@@ -0,0 +1,15 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+  constructor(item,){
+  this.itemList = il
+  this.itemQuantity = iq
+
+  this.addItem = function(i,q){
+    this.itemList.push (i)
+    
+    }
+  }
+  //Type the instance functions below this comment.
+
+
+}
---
+++ item.js
@@ -0,0 +1,13 @@
+class Item{
+  //Fill in the item class below this comment.
+  constructor(n,p,s){
+  this.name = n;
+  this.price = p;
+  this.shipping = s;
+  }
+}
+
+//Create your three test items below this comment.
+myItem = new Item("Excalibur",5000,1)
+noahsItem = new Item("Gpu",700,3)
+flemItem = new Item("Meme",20,2)
