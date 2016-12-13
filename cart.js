ab5e234e80d8147fb75649e64f1ecab69c80a277
7rolmstead <7rolmstead@stpatrick.org>
2016-12-13 10:50:15 -0600

    working on another function

--- cart.js
+++ cart.js
@@ -1,15 +1,15 @@
 class Cart{
   //What is the first part of every class? Type it below.
-  constructor(item,){
+  constructor(il,iq){
   this.itemList = il
   this.itemQuantity = iq
-
-  this.addItem = function(i,q){
-    this.itemList.push (i)
-    
-    }
   }
   //Type the instance functions below this comment.
-
-
+  this.addItem = function(i,q){
+    this.itemList.push(i);
+    this.itemQuantity.push(q);
+  }
+  this.totalCart = function(){
+    let total = this.itemList * this,itemQuantity;
+  }
 }
